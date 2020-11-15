52b00c438a823feae3d28f9da85f0b5ea0458341
tigerich <tigerich@163.com>
2020-11-16 00:20:58 +0800

    update default value for bp list quantity

--- contracts/mgp.bpvoting/include/mgp.bpvoting/bpvoting_entities.hpp
+++ contracts/mgp.bpvoting/include/mgp.bpvoting/bpvoting_entities.hpp
@@ -51,8 +51,8 @@ struct [[eosio::table("global"), eosio::contract("mgp.bpvoting")]] global_t {
         max_bp_size                     = 21;
         max_candidate_size              = 30;
         refund_time                     = 3 * 24 * 3600; //3-days in sec
-        min_bp_list_quantity            = asset(10'0000ll, SYS_SYMBOL);
-        min_bp_accept_quantity          = asset(20'0000ll, SYS_SYMBOL);
+        min_bp_list_quantity            = asset(100'000'0000ll, SYS_SYMBOL);
+        min_bp_accept_quantity          = asset(200'000'0000ll, SYS_SYMBOL);
         total_listed                    = asset(0, SYS_SYMBOL);
         total_staked                    = asset(0, SYS_SYMBOL);
         total_rewarded                  = asset(0, SYS_SYMBOL);
