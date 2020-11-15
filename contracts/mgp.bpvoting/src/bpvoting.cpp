783935b6982248488602c335f9d892ad776c3178
tigerich <tigerich@163.com>
2020-11-15 23:20:17 +0800

    update available primary key by plus 1

--- contracts/mgp.bpvoting/src/bpvoting.cpp
+++ contracts/mgp.bpvoting/src/bpvoting.cpp
@@ -245,11 +245,11 @@ void mgp_bpvoting::deposit(name from, name to, asset quantity, string memo) {
 	}
 	
 	//all other cases will be handled as rewards
-	reward_t reward(_dbc.get_pk<reward_t>());
+	reward_t reward(_dbc.get_pk<reward_t>() + 1);
 	reward.quantity = quantity;
 	reward.created_at = current_time_point();
 	_dbc.set( reward );
-	
+
 	_gstate.total_rewarded += quantity;
 
 }
