9ea7129735fb41861c6364f57e530beefd989961
tigerich <tigerich@163.com>
2020-11-15 22:54:00 +0800

    fix wasmdb set

--- contracts/mgp.bpvoting/include/mgp.bpvoting/wasm_db.hpp
+++ contracts/mgp.bpvoting/include/mgp.bpvoting/wasm_db.hpp
@@ -15,37 +15,37 @@ enum return_t{
 
 class dbc {
 private: 
-    name db_code;   //contract owner
+    name code;   //contract owner
 
 public: 
-    dbc(const name& code): db_code(code) {}
+    dbc(const name& code): code(code) {}
 
     template<typename RecordType>
     bool get(RecordType& record) {
-        // typename RecordType::table_t tbl(db_code, record.scope());
-         typename RecordType::table_t tbl(db_code, db_code.value);
+        // typename RecordType::table_t tbl(code, record.scope());
+         typename RecordType::table_t tbl(code, code.value);
         return( tbl.find(record.primary_key()) != tbl.end() );
     }
 
     template<typename RecordType>
     bool get_pk() {
-        typename RecordType::table_t tbl(db_code, db_code.value);
+        typename RecordType::table_t tbl(code, code.value);
         return( tbl.available_primary_key() );
     }
 
     template<typename RecordType>
     return_t set(const RecordType& record) {
-        typename RecordType::table_t tbl(db_code, record.scope());
-        // typename RecordType::table_t tbl(db_code, db_code.value);
+        typename RecordType::table_t tbl(code, record.scope());
+        // typename RecordType::table_t tbl(code, code.value);
 
         auto itr = tbl.find( record.primary_key() );
         if ( itr != tbl.end()) {
-            tbl.modify( itr, same_payer, [&]( auto& s ) {
+            tbl.modify( itr, code, [&]( auto& s ) {
                 s = record;
             });
             return return_t::MODIFIED;
         } else {
-            tbl.emplace( same_payer, [&]( auto& s ) {
+            tbl.emplace( code, [&]( auto& s ) {
                 s = record;
             });
             return return_t::APPENDED;
@@ -54,8 +54,8 @@ public:
 
     template<typename RecordType>
     void del(const RecordType& record) {
-        // typename RecordType::table_t tbl(db_code, record.scope());
-        typename RecordType::table_t tbl(db_code, db_code.value);
+        // typename RecordType::table_t tbl(code, record.scope());
+        typename RecordType::table_t tbl(code, code.value);
         auto itr = tbl.find(record.primary_key());
         if ( itr != tbl.end() ) {
             tbl.erase(itr);
