4f63e1f2a637b6dd2a31fe07c83f5be21afd529e
Raymond Chen <tigerich@163.com>
2020-11-20 14:12:21 +0000

    add unittest scripts

---
+++ unittest.sh
@@ -0,0 +1,56 @@
+function deploy {
+    cd /opt/src/MGP-Contracts && cleos set contract $con ./build/contracts/mgp.bpvoting/
+    cleos set account permission $con active '{"threshold": 1,"keys": [{"key": "'$pubkey'","weight": 1}],"accounts": [{"permission":{"actor":"'$con'","permission":"eosio.code"},"weight":1}]}' owner -p $con
+
+}
+function init {
+  echo "0. conf & init"
+  echo ""
+  cleos push action $con config '[20,30,50,21,180,600,1,"100.0000 MGP","200.0000 MGP","10.0000 MGP"]' -p $con
+  cleos push action $con init '[]' -p $con
+}
+
+function reward {
+  echo "1. send rewards"
+  echo ""
+  cleos transfer eosio $con "1000.0000 MGP" 
+  cleos transfer eosio $con "1000.0000 MGP" 
+  cleos transfer eosio $con "50.0000 MGP" 
+}
+
+function list {
+  echo "2. list masteraychen, richard.chen, raymond.chen"
+  echo ""
+  cleos transfer masteraychen $con "1000.0000 MGP" "list:2000"
+  cleos transfer richard.chen $con "200.0000 MGP" "list:3000"
+  cleos transfer raymond.chen $con "3000.0000 MGP" "list:5000"
+}
+
+function vote {
+  echo "3. cast votes..."
+  echo ""
+  cleos transfer eosio $con "100.0000 MGP" "vote:masteraychen"
+  cleos transfer eosio $con "200.0000 MGP" "vote:richard.chen"
+  cleos transfer eosio $con "300.0000 MGP" "vote:raymond.chen"
+}
+
+function exec {
+  echo "4. execute election..."
+  echo ""
+  cleos push action $con execute '[]' -p masteraychen
+}
+
+
+. ./env
+
+level=$1
+[ -z "$level" ] && level=0
+
+[ $level == "deploy" ] && deploy
+[ $level == 0 ] && init
+[ $level == 1 ] && reward
+[ $level == 2 ] && list
+[ $level == 3 ] && vote
+[ $level == 4 ] && exec
+
+
