430aecbccf2b2c0fa075a51c457513525ebc738e
tigerich <tigerich@163.com>
2020-12-03 18:08:06 +0800

    use reward_round for execute rewards

--- contracts/mgp.bpvoting/src/bpvoting.cpp
+++ contracts/mgp.bpvoting/src/bpvoting.cpp
@@ -163,12 +163,12 @@ void mgp_bpvoting::_tally_votes(election_round_t& last_round, election_round_t& 
 		if (mons > 0) {
 			votes.modify( *itr, _self, [&]( auto& row ) {
 				row.restarted_at += seconds(mons * 30 * seconds_per_day);
-				row.election_round = execution_round.round_id;
+				row.reward_round = execution_round.round_id;
 			});
 		}
 
-		if (itr->election_round >= execution_round.round_id) continue;
-		auto age = execution_round.round_id - itr->election_round;
+		if (itr->reward_round >= execution_round.round_id) continue;
+		auto age = execution_round.round_id - itr->reward_round;
 		auto coinage = itr->quantity * age;
 		execution_round.total_votes_in_coinage += coinage;
 		_dbc.set( execution_round );
@@ -209,8 +209,8 @@ void mgp_bpvoting::_tally_unvotes(election_round_t& round) {
 		voter_t voter(itr->owner);
 		check( _dbc.get(voter), "Err: voter not found" );
 
-		if (itr->election_round >= round.round_id) continue;
-		auto age = round.round_id - itr->election_round;
+		if (itr->reward_round >= round.round_id) continue;
+		auto age = round.round_id - itr->reward_round;
 		auto coinage = itr->quantity * age;
 		round.total_votes_in_coinage -= coinage;
 		round.unvote_count++;
@@ -294,8 +294,8 @@ void mgp_bpvoting::_execute_rewards(election_round_t& round) {
 		voter_t voter(itr->owner);
 		check( _dbc.get(voter), "Err: voter not found" );
 
-		if (itr->election_round >= round.round_id) continue;
-		auto age = round.round_id - itr->election_round;
+		if (itr->reward_round >= round.round_id) continue;
+		auto age = round.round_id - itr->reward_round;
 		auto coinage = itr->quantity * age;
 		auto voter_rewards = round.elected_bps[itr->candidate].allocated_voter_rewards * coinage.amount / round.total_votes_in_coinage.amount;
 		voter.unclaimed_rewards += voter_rewards;
